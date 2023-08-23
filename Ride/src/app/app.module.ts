50158627e696c1767cac86b133f8d40508df64d2
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-08-23 08:13:35 +0300

    st:heading-styling - optimize heading styling

--- Ride/src/app/app.module.ts
+++ Ride/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { HomeComponent } from './public/home/home.component';
 import { CustomsComponent } from './public/customs/customs.component';
-import { PartsComponent } from './parts/parts.component';
+import { PartsComponent } from './public/parts/parts.component';
 
 
 @NgModule({
