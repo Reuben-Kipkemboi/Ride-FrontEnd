aafd2cfd1fa492d9e969e1af300bccfbf0f03ddf
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-10-15 11:34:10 +0300

    ft:nav-bar routing - add testimonials routing

--- Ride/src/app/app-routing.module.ts
+++ Ride/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { CustomsComponent } from './public/customs/customs.component';
 import { PartsComponent } from './public/parts/parts.component';
+import { TestimonialsComponent } from './public/testimonials/testimonials.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'customs', component: CustomsComponent },
   { path: 'parts', component: PartsComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
   { path: '**', redirectTo: '' },
  
 ];
