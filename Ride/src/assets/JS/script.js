8e2d257a561c5d0ea40c5ed5cfdfa8293f6a7335
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-10-15 09:50:36 +0300

    st:card-styling - optimize car styling

--- Ride/src/assets/JS/script.js
+++ Ride/src/assets/JS/script.js
@@ -6,5 +6,71 @@ $(document).ready(function() {
     $("#price-card1").mouseout(function () {
         $(this).find('.part-description, .part-price').hide();
     });
+
+    $("#price-card2").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card2").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+    $("#price-card3").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card3").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+    $("#price-card4").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card4").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+
+    $("#price-card5").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card5").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+    $("#price-card6").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card6").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+    $("#price-card7").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card7").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+    $("#price-card8").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card8").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
+
+
+    $("#price-card9").mouseover(function () {
+        $(this).find('.part-description, .part-price').show();
+    });
+
+    $("#price-card9").mouseout(function () {
+        $(this).find('.part-description, .part-price').hide();
+    });
 });
 
